d12487c7244f6b1ac98e9763f5a68d916eceaa15
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-05-18 17:37:52 +0100

    Update to typescript version of jellyfish-core module
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,6 +4,10 @@
  * Proprietary and confidential.
  */
 
+import { ActionFile } from '@balena/jellyfish-plugin-base';
+import { ContractData } from '@balena/jellyfish-types/build/core';
 import { actionBootstrapChannel } from './action-bootstrap-channel';
 
-export const actions = [actionBootstrapChannel];
+export const actions: Array<ActionFile<ContractData>> = [
+	actionBootstrapChannel,
+];
