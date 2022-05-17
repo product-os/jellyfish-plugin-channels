db50aa386e3ded861a871506a4bb9e04f3f41fa6
Josh Bowling <josh@balena.io>
2022-05-17 14:03:32 +0900

    Add agent-channel-settings contract
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,9 +1,11 @@
 import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
+import { agentChannelSettings } from './agent-channel-settings';
 import { channel } from './channel';
 import { relationshipViewIsAttachedToChannel } from './relationship-view-is-attached-to-channel';
 import { triggeredActionBootstrapChannel } from './triggered-action-bootstrap-channel';
 
 export const contracts: ContractDefinition[] = [
+	agentChannelSettings,
 	channel,
 	relationshipViewIsAttachedToChannel,
 	triggeredActionBootstrapChannel,
