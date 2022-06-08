2096a3d882f665a57320c8c13552f14655ad21c8
Josh Bowling <josh@balena.io>
2022-06-08 15:45:41 +0900

    Add agent relationships
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,12 +1,18 @@
 import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
 import { agentChannelSettings } from './agent-channel-settings';
 import { channel } from './channel';
+import { relationshipChannelHasAgentUser } from './relationship-channel-has-agent-user';
+import { relationshipChannelHasSettingsAgentChannelSettings } from './relationship-channel-has-settings-agent-channel-settings';
+import { relationshipUserHasSettingsAgentChannelSettings } from './relationship-user-has-settings-agent-channel-settings';
 import { relationshipViewIsAttachedToChannel } from './relationship-view-is-attached-to-channel';
 import { triggeredActionBootstrapChannel } from './triggered-action-bootstrap-channel';
 
 export const contracts: ContractDefinition[] = [
 	agentChannelSettings,
 	channel,
+	relationshipChannelHasAgentUser,
+	relationshipChannelHasSettingsAgentChannelSettings,
+	relationshipUserHasSettingsAgentChannelSettings,
 	relationshipViewIsAttachedToChannel,
 	triggeredActionBootstrapChannel,
 ];
